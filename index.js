d9aac3e3a8b631062ab58ccd24c97bc76e35e71c
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-15 14:20:21 -0700

    added index.js and server.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 applicationCache.use(cors())
+const app = require('./server.js');
+const port = process.env.PORT || 4582; 
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
