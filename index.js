d283181182b485a9deb9f02605e49393e5617911
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-15 16:02:33 -0700

    added working test

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
+const app = require('./server.js');
 const port = process.env.PORT || 4582;
-const express = require('express');
-const magicItems = require('./data.js');
-const cors = require('cors');
-const app = express();
-app.use(cors()); 
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-app.get('/magicItems', (req, res) => {
-    res.json(magicItems);
-});
-
-app.get('/magicItems/:id', (req, res) => {   
-    const id = Number(req.params.id);
-
-    const item = magicItems.find((singleItem) => singleItem.id === id);
-    res.json(item);
-});
-
-module.exports = app;
